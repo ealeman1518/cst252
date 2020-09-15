5836b9e7e18d34043cf320deb0243a49d351910a
ealeman1518 <ealeman@csumb.edu>
2020-09-14 20:08:23 -0700

    Updates for Lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,21 @@
+/*
+  * Author: Eduardo Aleman
+  * Created: September, 14 2020
+  * License: Public Domain
+  */
+
+  // Defining Variables
+make = "Nissan";
+model = "Versa";
+color = "Black";
+year = "2013";
+
+  // calculations
+age = 2020 - year;
+
+  // output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>")
