0af2e897b6e44a4092ced716a1c0b2141871e610
ealeman1518 <ealeman@csumb.edu>
2020-11-08 22:31:09 -0800

    upodates

--- lab19/js/lab.js
+++ lab19/js/lab.js
@@ -6,16 +6,33 @@
 
 
 
-// $("test").load("http://numbersapi.com/5/math", callback)
-//
-$.ajax({
-  url: "http://numbersapi.com/5/math",
-  data: {id: 123},
-  type: "GET",
-  dataType: "json",
-});
+math = "http://numbersapi.com/5/math"
+
+// $.ajax({
+//   url: math,
+//   data: {id: 123},
+//   type: "GET",
+//   dataType: "json",
+// });
 
 $("#activate").click(function() {
-  $("test").load("http://numbersapi.com/5/math");
   console.log("Click");
+
+  $.ajax({
+    url: math,
+    data: {id: 123},
+    type: "GET",
+    dataType: "json",
+  })
+
+  //Taken from the jsfiddle example during class lecture on AJAX by Wes Modes
+  .done(function( data ) {
+      // alert("Success!");
+      var quote = data.message;
+      $("#output").append("<p>" + quote)
+  })
+  // If the request fails
+  .fail(function( xhr, status, errorThrown ) {
+      console.log(errorThrown + " Status:" + status );
+	})
 });
